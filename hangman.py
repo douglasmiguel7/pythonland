493a5a4cb00854602b6f42c710f5850bfc3501b6
Douglas Miguel <douglasandrademiguel@gmail.com>
2020-08-07 16:09:52 -0300

    organizado codigo do jogo da forca

--- hangman.py
+++ hangman.py
@@ -1,10 +1,12 @@
 import random
 
 
-def start():
+def show_welcome_message():
     print("Bem vindo ao jogo da forca!")
     print()
 
+
+def get_secret_word():
     file = open("fruits.txt", mode="r", encoding="UTF-8")
 
     secret_words = []
@@ -19,32 +21,112 @@ def start():
     secret_word_number = random.randrange(0, len(secret_words))
 
     secret_word = secret_words[secret_word_number]
-    correct_letters = ["_" for secret_letter in secret_word]
 
-    hanged = False
-    won = False
-    amount_errors = 0
+    return secret_word
+
+
+def show_handman_state(amount_errors, correct_letters):
+    if (amount_errors == 0):
+        print("  _______   ")
+        print(" |       !  ")
+        print(" |          ")
+        print(" |          ")
+        print(" |          ")
+        print(" |          ")
+        print(" |          ")
+
+    elif (amount_errors == 1):
+        print("  _______   ")
+        print(" |       !  ")
+        print(" |      (_) ")
+        print(" |          ")
+        print(" |          ")
+        print(" |          ")
+        print(" |          ")
+
+    elif (amount_errors == 2):
+        print("  _______   ")
+        print(" |       !  ")
+        print(" |      (_) ")
+        print(" |       |  ")
+        print(" |       |  ")
+        print(" |          ")
+        print(" |          ")
+
+    elif (amount_errors == 3):
+        print("  _______   ")
+        print(" |       !  ")
+        print(" |      (_) ")
+        print(" |      /|  ")
+        print(" |       |  ")
+        print(" |          ")
+        print(" |          ")
+
+    elif (amount_errors == 4):
+        print("  _______   ")
+        print(" |       !  ")
+        print(" |      (_) ")
+        print(" |      /|\ ")
+        print(" |       |  ")
+        print(" |          ")
+        print(" |          ")
+
+    elif (amount_errors == 5):
+        print("  _______   ")
+        print(" |       !  ")
+        print(" |      (_) ")
+        print(" |      /|\ ")
+        print(" |       |  ")
+        print(" |      /   ")
+        print(" |          ")
+
+    elif (amount_errors == 6):
+        print("  _______   ")
+        print(" |       !  ")
+        print(" |      (_) ")
+        print(" |      /|\ ")
+        print(" |       |  ")
+        print(" |      / \ ")
+        print(" |          ")
 
     correct_word = ""
 
     for correct_letter in correct_letters:
         correct_word = correct_word + correct_letter + " "
 
-    print(" _______   ")
-    print("|       !  ")
-    print("|          ")
-    print("|          ")
-    print("|          ")
-    print("|          ")
-    print("|          ")
-    print("| {}".format(correct_word))
+    print(" | {}".format(correct_word))
     print()
 
+
+def show_tip(secret_word):
+    print("Dica: é uma fruta com {} letras".format(len(secret_word)))
+
+
+def request_user_guess():
+    user_guess = input("Digite uma letra: ")
+    user_guess = user_guess.upper()
+    print()
+
+    return user_guess
+
+
+def start():
+    show_welcome_message()
+
+    secret_word = get_secret_word()
+
+    correct_letters = ["_" for secret_letter in secret_word]
+
+    hanged = False
+    won = False
+    amount_errors = 0
+
+    show_handman_state(amount_errors, correct_letters)
+
     while (not hanged and not won):
-        print("Dica: é uma fruta com {} letras".format(len(secret_word)))
-        user_guess = input("Digite uma letra: ")
-        user_guess = user_guess.upper()
-        print()
+        show_tip(secret_word)
+
+        user_guess = request_user_guess()
 
         if (not user_guess in secret_word):
             amount_errors = amount_errors + 1
@@ -53,91 +135,12 @@ def start():
         for letter in secret_word:
             user_guess_is_correct = user_guess == letter
 
-            if   (user_guess_is_correct):
+            if (user_guess_is_correct):
                 correct_letters[letter_position] = letter
 
             letter_position = letter_position + 1
 
-        correct_word = ""
-
-        for correct_letter in correct_letters:
-            correct_word = correct_word + correct_letter + " "
-
-        if (amount_errors == 0):
-            print("  _______   ")
-            print(" |       !  ")
-            print(" |          ")
-            print(" |          ")
-            print(" |          ")
-            print(" |          ")
-            print(" |          ")
-            print(" | {}".format(correct_word))
-            print()
-
-        elif (amount_errors == 1):
-            print("  _______   ")
-            print(" |       !  ")
-            print(" |      (_) ")
-            print(" |          ")
-            print(" |          ")
-            print(" |          ")
-            print(" |          ")
-            print(" | {}".format(correct_word))
-            print()
-
-        elif (amount_errors == 2):
-            print("  _______   ")
-            print(" |       !  ")
-            print(" |      (_) ")
-            print(" |       |  ")
-            print(" |       |  ")
-            print(" |          ")
-            print(" |          ")
-            print(" | {}".format(correct_word))
-            print()
-
-        elif (amount_errors == 3):
-            print("  _______   ")
-            print(" |       !  ")
-            print(" |      (_) ")
-            print(" |      /|  ")
-            print(" |       |  ")
-            print(" |          ")
-            print(" |          ")
-            print(" | {}".format(correct_word))
-            print()
-        elif (amount_errors == 4):
-            print("  _______   ")
-            print(" |       !  ")
-            print(" |      (_) ")
-            print(" |      /|\ ")
-            print(" |       |  ")
-            print(" |          ")
-            print(" |          ")
-            print(" | {}".format(correct_word))
-            print()
-
-        elif (amount_errors == 5):
-            print("  _______   ")
-            print(" |       !  ")
-            print(" |      (_) ")
-            print(" |      /|\ ")
-            print(" |       |  ")
-            print(" |      /   ")
-            print(" |          ")
-            print(" | {}".format(correct_word))
-            print()
-
-        elif (amount_errors == 6):
-            print("  _______   ")
-            print(" |       !  ")
-            print(" |      (_) ")
-            print(" |      /|\ ")
-            print(" |       |  ")
-            print(" |      / \ ")
-            print(" |          ")
-            print(" | {}".format(correct_word))
-            print()
+        show_handman_state(amount_errors, correct_letters)
 
         won = not "_" in correct_letters
         hanged = amount_errors == 6
